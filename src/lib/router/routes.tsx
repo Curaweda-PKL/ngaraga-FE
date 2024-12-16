4502e1d428557319100ef14cd09f9a5deb019d64
Lietreum <mirsieolien@gmail.com>
2024-12-16 11:15:04 +0700

    feat: reset-password, check email, forgot password, heroframe responsivneness

--- src/lib/router/routes.tsx
+++ src/lib/router/routes.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import type {PathRouteProps} from "react-router-dom";
-
+const ForgotPassword = React.lazy(() => import("@/lib/pages/auth/login/forgot-password"))
+const ResetPassword = React.lazy(() => import("@/lib/pages/auth/login/reset-password"))
 const Home = React.lazy(() => import("@/lib/pages/home/Home"));
 const Signup = React.lazy(() => import("@/lib/pages/auth/login/signup"));
 const Login = React.lazy(() => import("@/lib/pages/auth/login/login"));
 const Artists = React.lazy(() => import("@/lib/pages/artists/artists-index"));
-const Marketplace = React.lazy(
-  () => import("@/lib/pages/marketplace/marketplace-index")
-);
-const TopCollectors = React.lazy(
-  () => import("@/lib/pages/top-players/top-players")
-);
+const Marketplace = React.lazy(() => import("@/lib/pages/marketplace/marketplace-index"));
+const TopCollectors = React.lazy(() => import("@/lib/pages/top-players/top-players"));
 const Detail = React.lazy(() => import("@/lib/pages/detail-card/detail-index"));
 const Cart = React.lazy(() => import("@/lib/pages/cart/mycart"));
 
@@ -26,6 +23,14 @@ export const routes: Array<PathRouteProps> = [
   {
     path: "/login",
     element: <Login />,
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword/>
+  },
+  {
+    path: "/reset-password",
+    element: <ResetPassword/>
   },
   {
     path: "/artists",
@@ -47,6 +52,7 @@ export const routes: Array<PathRouteProps> = [
     path: "/cart",
     element: <Cart />,
   },
+  
 ];
 
 export const privateRoutes: Array<PathRouteProps> = [];
