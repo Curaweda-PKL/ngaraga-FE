9b5efad2adac781da2390b0c185cb8a87819f962
Lietreum <mirsieolien@gmail.com>
2025-02-28 13:26:23 +0700

    section4

--- src/lib/admin-pages/pages/Home/home.tsx
+++ src/lib/admin-pages/pages/Home/home.tsx
@@ -1,11 +1,11 @@
-import {HeroBanner} from "./components/section-1";
-import {SectionTwoForm} from "./components/section-2";
-import {SectionThreeForm} from "./components/section-3";
-import {SectionFourForm} from "./components/section-4";
-import {SectionFiveForm} from "./components/section-5";
-import {SectionSixForm} from "./components/section-6";
-import {SectionSevenForm} from "./components/section-7";
-import {SectionEightForm} from "./components/section-8";
+import { HeroBanner } from "./components/section-1";
+import { SectionTwoForm } from "./components/section-2";
+import { SectionThreeForm } from "./components/section-3";
+import { SectionFourForm } from "./components/section-4";
+import { SectionFiveForm } from "./components/section-5";
+import { SectionSixForm } from "./components/section-6";
+import { SectionSevenForm } from "./components/section-7";
+import { SectionEightForm } from "./components/section-8";
 
 // Breadcrumb Component
 const Breadcrumb = () => {
@@ -26,17 +26,35 @@ const Breadcrumb = () => {
 // Modified Home Section Component
 export const HomeSection = () => {
   return (
-    <div className="text-[#262626]  ">
+    <div className="text-[#262626]">
       <Breadcrumb />
-      <HeroBanner />
-      <SectionTwoForm  />
-      <SectionThreeForm />
-      <SectionFourForm />
-      <SectionFiveForm />
-      <SectionSixForm />
-      <SectionSevenForm />
-      <SectionEightForm />
-      <ActionButtons />
+      <div className="-mt-2">
+        <HeroBanner />
+      </div>
+      <div className="mt-8">
+        <SectionTwoForm />
+      </div>
+      <div className="mt-8">
+        <SectionThreeForm />
+      </div>
+      <div className="mt-8">
+        <SectionFourForm />
+      </div>
+      <div className="mt-8">
+        <SectionFiveForm />
+      </div>
+      <div className="mt-8">
+        <SectionSixForm />
+      </div>
+      <div className="mt-8">
+        <SectionSevenForm />
+      </div>
+      <div className="mt-8">
+        <SectionEightForm />
+      </div>
+      <div className="mt-8">
+        <ActionButtons />
+      </div>
     </div>
   );
 };
